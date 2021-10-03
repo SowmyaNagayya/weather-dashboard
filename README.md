c4ede6b24c9f915a7a960bc12f5d0a4cb0afa5ac
Sowmya Nagayya <kkd.sowmya@gmail.com>
2021-10-02 22:55:32 -0500

    final code

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# weather-dashboard
+# Homework 5:weather-dashboard
+
+## objectives
+ 
+Created weather-dashboard app to see the weather outlook for multiple cities for current weather forecast and 5days forecast
+SO THAT we can plan a trip accordingly.
+``` 
+
+## Screenshot of homepage
+
+![Here is a screen shot of the final page/homepage.](./assets/image/weatherdashboard.png)
+
+
+## Link to deployed page
+
+You can also refer to this [GitHub Pages](https://github.com/SowmyaNagayya/codequiz).
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
+//selectors
 var searchButton = $("#searchbutton");
 var inputField = $("#searchinput");
 var cityNameStorageField =[];
+var uviIndex = $("#UVIndex");
 
-
+// search button click
 searchButton.click(function(event) {
   event.preventDefault();
   var city = inputField.val(); 
@@ -16,11 +18,21 @@ function getAPI(city) {
   fetch(requestURL)
     .then(function(response) {
        response.json().then(function (data) {
-         $("#cityname").text(data["name"]);
+         console.log(data);
+         $("#cityname").html(data["name"]+moment(data.dt,"X").format("( MM/DD/YYYY )") +"<img src='http://openweathermap.org/img/w/"+data.weather[0].icon+".png'/>");
          $("#temp").text("Temp: " + data["main"].temp + " F");
          $("#wind").text("Wind: " + data["wind"].speed + " mph");
          $("#humidity").text("humidity: " + data["main"].humidity+ " %");
 
+
+         fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude=hourly,daily&appid=8cabcf2bb69158502474a06c66719777`)
+         .then(function(response) {
+           return response.json()
+         })
+         .then(function(uvdata) {
+             console.log(uvdata);
+             uviIndex.html(uvdata.current.uvi);
+         })
         // Updating the local storage
         updateStorage(data["name"]);
 
@@ -58,6 +70,7 @@ function futureForecast(cityLatitude, cityLongitude){
       console.log(data.length);
       console.log(data);
       for(var i=0; i<5;i++){
+        $("#date"+i).html(moment(data.daily[i].dt,"X").format("( MM/DD/YYYY )") +"<img src='http://openweathermap.org/img/w/"+data.daily[i].weather[0].icon+".png'/>");
         $("#temp"+i).text("Temp: " + data.daily[i].temp.day + " F");
         $("#wind"+i).text("Wind: " + data.daily[i].wind_speed + " mph");
         $("#humidity"+i).text("humidity: " + data.daily[i].humidity+ " %");
